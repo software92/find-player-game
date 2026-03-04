0010d266f1d26514c5ca77eb766ddf0196964606
jsw <csw9201@naver.com>
2026-03-04 17:32:56 +0900

    refactor: `indext.ts` 렌더 트리 수정
    - `<StrictMode>` 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,18 @@ import App from './App'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { RecoilRoot } from 'recoil'
 import { HelmetProvider } from 'react-helmet-async'
-
-const root = createRoot(document.getElementById('root') as HTMLElement)
+import { StrictMode } from 'react'
 
 const queryClient = new QueryClient()
 
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <HelmetProvider>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
-    </HelmetProvider>
-  </QueryClientProvider>,
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </HelmetProvider>
+    </QueryClientProvider>
+  </StrictMode>,
 )
