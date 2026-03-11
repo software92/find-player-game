6de2d93355f28c1ce6388cf8fe08ed856e5b9112
jsw <csw9201@naver.com>
2026-03-11 18:12:54 +0900

    feat: `styled-components` theme 적용
    - `@types/styled-components` 설치
    - `<ThemeProvider theme=[]>` 적용
    - theme 파일 및 타입 선언 파일 생성(`theme.ts`, `styled.d.ts`)

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { syncFirebase } from './services/syncService'
 import { StrictMode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import BrowserRouter from './BrowserRouter'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme'
 
 const queryClient = new QueryClient()
 
@@ -13,9 +15,11 @@ syncFirebase()
 createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <HelmetProvider>
-      <RecoilRoot>
-        <BrowserRouter />
-      </RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <RecoilRoot>
+          <BrowserRouter />
+        </RecoilRoot>
+      </ThemeProvider>
     </HelmetProvider>
   </QueryClientProvider>,
 )
