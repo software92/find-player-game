a5b09d35b05f326134810c689b4406d0282eff07
jsw <csw9201@naver.com>
2026-03-19 15:14:02 +0900

    deploy: `router`, vite config에 basename/base 입력

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -24,6 +24,6 @@ const routes = [
   },
 ]
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes, { basename: '/find-player-game' })
 
 export default router
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/find-player-game/',
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
