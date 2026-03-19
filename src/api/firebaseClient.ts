df039fa67fd35866c6b1f6bd91dc2b27d3a6c20f
jsw <csw9201@naver.com>
2026-03-19 15:07:28 +0900

    chore: 일부 환경변수를 문자열로 직접 사용
    - api key를 제외한 config 옵션은 문자열로 직접 입력

--- src/api/firebaseClient.ts
+++ src/api/firebaseClient.ts
@@ -2,12 +2,13 @@
 import axios from 'axios'
 
 const FIREBASE_API_CONFIG = {
-  FIREBASE_API_BASE_URL: import.meta.env.VITE_FIREBASE_API_BASE_URL,
+  FIREBASE_API_BASE_URL:
+    'https://real-time-db--find-player-default-rtdb.firebaseio.com',
   FIREBASE_API_HEADERS: { 'Content-Type': 'application/json' },
 } as const
 
 export const firebaseApiInstance = axios.create({
-  baseURL: `${FIREBASE_API_CONFIG.FIREBASE_API_BASE_URL}`,
+  baseURL: FIREBASE_API_CONFIG.FIREBASE_API_BASE_URL,
   headers: FIREBASE_API_CONFIG.FIREBASE_API_HEADERS,
 })
 
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,12 +5,12 @@ import { getDatabase } from 'firebase/database'
 // Replace these values with your Firebase project configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  authDomain: 'real-time-db--find-player.firebaseapp.com',
+  databaseURL: 'https://real-time-db--find-player-default-rtdb.firebaseio.com',
+  projectId: 'real-time-db--find-player',
+  storageBucket: 'real-time-db--find-player.firebasestorage.app',
+  messagingSenderId: '193465948749',
+  appId: '1:193465948749:web:9e6868f1ba03a50b225a04',
 }
 
 // Initialize Firebase with your configuration
